e272a3b3518c6784bdc0e9dcf8c11299ccf3d6c3
mibelcal <mibelcal@yahoo.es>
2024-03-04 19:41:36 +0100

    primera carga

---
+++ src/main/java/com/microcompany/proyectosytareas/ProyectosyTareas/ProyectosyTareasApplication.java
@@ -0,0 +1,13 @@
+package com.microcompany.proyectosytareas.ProyectosyTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectosyTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectosyTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microcompany/proyectosytareas/ProyectosyTareas/ProyectosyTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcompany.proyectosytareas.ProyectosyTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectosyTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
